89ed992d242d0db89af9197603cf59329abdbc59
davinstewart <me@davinstewart.com>
2018-12-31 12:03:39 -0500

    setup.bat

---
+++ setup.bat
@@ -0,0 +1,10 @@
+# Create a blank git repo somewhere (I use github)
+
+# Clone the repo down to my local drive
+git clone <repo url from github>
+
+cd <the folder that was created from git clone>
+
+npm init
+
+npm install express request --save
